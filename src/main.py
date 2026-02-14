7af0af9fb48870c604a23f32c996e7fdb84d56db
Nelli Skogman <nelli.skogman@gmail.com>
2026-02-14 20:39:25 +0100

    Set up spotipy

--- src/main.py
+++ src/main.py
@@ -1,4 +1,19 @@
-def main(): ...
+from dotenv import load_dotenv
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+
+
+def main():
+    load_dotenv()
+
+    scope: list[str] = ["playlist-read-private"]
+
+    spotify: Spotify = Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    result: dict[str, str] = spotify.current_user_playlists()
+
+    for playlist in result["items"]:
+        print(playlist["name"])
 
 
 if __name__ == "__main__":
